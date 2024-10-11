e83642911d8e76ba1ed0fb38ce9494b1d744642b
airybinov <jikafosters5@gmail.com>
2024-10-11 11:43:47 +0300

    сделал первый тест с матрицей

--- src/main/java/org/softwaretechnologies/ArrayFunctions.java
+++ src/main/java/org/softwaretechnologies/ArrayFunctions.java
@@ -10,8 +10,19 @@ public final class ArrayFunctions {
      * @param array массив, который будет перевернут.
      */
     public static void reverse(int[] array) {
-        // TODO: реализуйте вышеуказанную функцию
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left < right) {
+            int temp = array[left];
+            array[left] = array[right];
+            array[right] = temp;
+
+            left++;
+            right--;
+        }
     }
+
 
     /**
      * Функция, заменяющая строки матрицы на столбцы матрицы. Пример:
@@ -24,6 +35,25 @@ public final class ArrayFunctions {
      * @param matrix матрица, в которой столбцы будут заменены на строки.
      */
     public static void rotateMatrix(int[][] matrix) {
-        // TODO: реализуйте вышеуказанную функцию
+        int n = matrix.length;
+
+        // Проверка на квадратность матрицы
+        for (int i = 0; i < n; i++) {
+            if (matrix[i].length != n) {
+                System.out.println("Матрица не квадратная");
+                return;
+            }
+        }
+
+        // Транспонирование матрицы
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                // Обмен элементов
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
     }
+
 }
