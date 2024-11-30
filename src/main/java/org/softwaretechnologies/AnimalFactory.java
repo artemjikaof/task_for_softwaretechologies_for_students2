7a0377efa99739ee02b422f473ee870370b6c711
artemjikaof <jikafosters5@gmail.com>
2024-11-30 14:20:06 +0300

    30.11

--- src/main/java/org/softwaretechnologies/AnimalFactory.java
+++ src/main/java/org/softwaretechnologies/AnimalFactory.java
@@ -1,7 +1,6 @@
 package org.softwaretechnologies;
 
-import org.softwaretechnologies.animals.Animal;
-import org.softwaretechnologies.animals.AnimalType;
+import org.softwaretechnologies.animals.*;
 
 public class AnimalFactory {
 
@@ -15,7 +14,12 @@ public class AnimalFactory {
 
     public static Animal createAnimal(String name, AnimalType type) {
         /* TODO в зависимости от type создайте и верните нужное животное */
-
-        return null;
+        return switch (type)
+        {
+            case CAT -> new Cat(name);
+            case DOG -> new Dog(name);
+            case COW -> new Cow(name);
+            default -> throw new IllegalArgumentException("Unknown animal type: " + type);
+        };
     }
 }
--- src/main/java/org/softwaretechnologies/Zoo.java
+++ src/main/java/org/softwaretechnologies/Zoo.java
@@ -1,23 +1,20 @@
 package org.softwaretechnologies;
 
 import org.softwaretechnologies.animals.Animal;
-import org.softwaretechnologies.animals.AnimalType;
-
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class Zoo {
     private final List<Animal> animalList = new ArrayList<>();
-    public void addAnimal(Animal animal) {
 
+    public void addAnimal(Animal animal) {
         animalList.add(animal);
     }
 
     /**
-     * Метод должен возвращять список звуков животных.
-     * Звуки животных должны быть отсортированы по имени житного. Пример
+     * Метод должен возвращать список звуков животных.
+     * Звуки животных должны быть отсортированы по имени животного. Пример:
      * Животные:
      *
      * Корова: Яша
@@ -28,16 +25,18 @@ public class Zoo {
      * Кошка: Мурзик
      * Собака: Бобик
      *
-     * Вовращаемый список звуков: moo, woof, meow, woof, meow, woof, moo
+     * Возвращаемый список звуков: moo, woof, meow, woof, meow, woof, moo
      *
      * @return Звуки животных, в алфавитном порядке имени животного.
      */
     public List<String> soundAllAnimalsSortByName() {
         List<String> sounds = new ArrayList<>();
-        // TODO заполните корректно список звуков
+
+        // Сортировка animalList по имени животного
+        animalList.stream()
+                .sorted(Comparator.comparing(Animal::getName))
+                .forEach(animal -> sounds.add(animal.sound()));
 
         return sounds;
     }
-
-
 }
